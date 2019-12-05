e192db94dd30c85439b642ee8d2bf1878b19006a
Sophil-minn <1056640663@qq.com>
2019-12-05 10:33:04 +0800

    redux

--- jianshu/src/TodoList.js
+++ jianshu/src/TodoList.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import store from './store'
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    console.log(store.getState());
+  }
   render() {
     console.log('render');
     return (
       <div style={{ 'margin': '10px 0 0 10px' }}>
-        <Input placeholder="Minn  " style={{ 'width': '300px', 'marginRight': '10px' }} />
+        <Input value={this.state.inputValue} placeholder="Minn  " style={{ 'width': '300px', 'marginRight': '10px' }} />
         <Button type="primary">提交</Button>
         <List
           style={{ 'margin': '10px 0 0 0', 'width': '300px', }}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (
             <List.Item>
               {item}
---
+++ jianshu/src/store/reducer.js
@@ -0,0 +1,8 @@
+// 数据记录
+const defaultState = {
+  inputValue: '',
+  list: [1, 234, 435, 9, 67, 564]
+}
+export default (state = defaultState, action) => {
+  return state;
+}
