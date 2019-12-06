163f1976efc6c48259f331d076c766e09e2b1f7a
Sophil-minn <1056640663@qq.com>
2019-12-06 09:54:56 +0800

    精减代码

--- jianshu/src/TodoList.js
+++ jianshu/src/TodoList.js
@@ -1,59 +1,18 @@
 import React, { Component } from 'react';
-import store from './store'
-import TodoListUI from './TodoListUI'
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction, getInitList } from './store/actionCreators'
-// import axios from 'axios';
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-    // console.log(store.getState());
-    // 订阅store
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    store.subscribe(this.handleStoreChange);
-  }
   render() {
-    console.log('render');
-    return <TodoListUI
-      inputValue={this.state.inputValue}
-      list={this.state.list}
-      handleInputChange={this.handleInputChange}
-      handleBtnClick={this.handleBtnClick}
-      handleItemDelete={this.handleItemDelete}
-    />
-  }
-  componentDidMount() {
-    // const action = getTodoList();
-    // console.log(action);
-    // store.dispatch(action);
-    // axios.get('/todolist.json').then((res) => {
-    //   const data = res.data;
-    //   const action = initListAction(data);
-    //   console.log(data);
-    //   store.dispatch(action);
-    // });
-    const action = getInitList();
-    store.dispatch(action);
-    // console.log(action);
-  }
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value)
-    store.dispatch(action);
-  }
-  handleStoreChange() {
-    this.setState(store.getState());
-  }
-  handleBtnClick() {
-    const action = getAddItemAction()
-    store.dispatch(action);
-  }
-  handleItemDelete(index) {
-    const action = getDeleteItemAction(index)
-    store.dispatch(action);
+    return (
+      <div>
+        <div>
+          <input type="text" />
+          <button>提交</button>
+        </div>
+        <ul>
+          <li>Minn</li>
+        </ul>
+      </div>
+    )
   }
 }
 export default TodoList;
