f9c8fda1c258db66efb2369fa0df8dafac5df0ed
Sophil-minn <1056640663@qq.com>
2019-12-07 10:42:49 +0800

    首页专题区域布局

--- jianshu/src/pages/home/components/Topic.js
+++ jianshu/src/pages/home/components/Topic.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import { TopicWrapper, TopicItem } from '../style'
 class Topic extends Component {
   render() {
     return (
-      <div>Topic  </div>
+      <TopicWrapper>
+        <TopicItem>
+          <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/7663825/7c28763e-002b-4e89-8dea-5b8da210ef2c.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" alt="" />
+          社会热点
+        </TopicItem>
+
+      </TopicWrapper>
     )
   }
 }
