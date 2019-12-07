6645ac4570fffdfccf0c7da169c7e4fe9e3804ba
Sophil-minn <1056640663@qq.com>
2019-12-07 19:39:21 +0800

    页面路由参数传递

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -5,6 +5,7 @@ import { actionCreators } from './store';
 
 class Detail extends PureComponent {
   render() {
+    console.log(this.props);
     return (
       <DetailWrapper>
         <Header>{this.props.title}</Header>
